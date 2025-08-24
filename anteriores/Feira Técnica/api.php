f86e6e0403f7d6c983a0c74887c47f09b7b9fdcc
Amanda Leite <amandaleitedsm@gmail.com>
2025-08-24 18:29:40 -0300

    Delete anteriores directory

--- anteriores/Feira Técnica/api.php
+++
@@ -1,5 +0,0 @@
-<?php 
-    #date_default_timezone_set('America/Sao_Paulo');
-    #require_once "api/src/routes/Roteador.php";
-    #(new Roteador())->start(); // Start the router
-?>
--- anteriores/Feira Técnica/api/src/docs/banco/selectTables.sql
+++
@@ -1,7 +0,0 @@
-select * from planta_usuario;
-select * from cadastro_usuario;
-select * from analise_planta;
-select * from plantas;
-SELECT * FROM CONDICOES_PLANTA;
-select * from recomendacoes;
-select * from tokens_ativos;
